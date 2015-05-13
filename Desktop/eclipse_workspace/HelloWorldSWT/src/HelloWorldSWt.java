95d5d654d754501a4790a5e56e354361942df4d7
Z25Assassins <zkiam.a3992@gmail.com>
2015-05-12 19:15:03 -0600

    first commit

---
+++ Desktop/eclipse_workspace/HelloWorldSWT/src/HelloWorldSWt.java
@@ -0,0 +1,21 @@
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+
+
+public class HelloWorldSWt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Display display = new Display();
+		Shell shell = new Shell(display);
+		shell.setText("Hello world!");
+		shell.open();
+		while (!shell.isDisposed()) {
+		if (!display.readAndDispatch()) display.sleep();
+		}
+		display.dispose();
+
+	}
+
+}
